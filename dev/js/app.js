7c010d7bdcb0180426f7dd65dfa848e10b037e04
Harry <harryrock67@gmail.com>
2019-02-09 16:18:22 -0500

    completed css for app
    js is function for nav

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,4 +1,4 @@
 const openPage = what => {
     const wrapper = document.getElementsByTagName('wrapper')[0];
-    what == 'main' ? wrapper.style.transform = 'translateX(0)': what == 'adventure' ? wrapper.style.transform = 'translateX(-25%)': what == 'activity' ? wrapper.style.transform = 'translateX(-50%)' : wrapper.style.transform = 'translateX(-75%)';
+    what == 'memories' ? wrapper.style.transform = 'translateX(0)': what == 'main' ? wrapper.style.transform = 'translateX(-25%)': what == 'activity' ? wrapper.style.transform = 'translateX(-50%)' : wrapper.style.transform = 'translateX(-75%)';
 }
