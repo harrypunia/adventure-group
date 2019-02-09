5b76882ba74f7cc7ac8b703ec51a64d6032ac696
Harry <harryrock67@gmail.com>
2019-02-09 16:12:57 -0500

    added nav bar to change pages

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,6 +1,4 @@
-class App {
-    constructor() {
-    }
-    init() {
-    }
+const openPage = what => {
+    const wrapper = document.getElementsByTagName('wrapper')[0];
+    what == 'main' ? wrapper.style.transform = 'translateX(0)': what == 'adventure' ? wrapper.style.transform = 'translateX(-25%)': what == 'activity' ? wrapper.style.transform = 'translateX(-50%)' : wrapper.style.transform = 'translateX(-75%)';
 }
