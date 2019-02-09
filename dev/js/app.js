1b791e1e439d4526dcfd7b27e99af9a4c209c997
Harry <harryrock67@gmail.com>
2019-02-09 18:34:36 -0500

    check

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,3 +2,10 @@ const openPage = what => {
     const wrapper = document.getElementsByTagName('wrapper')[0];
     what == 'memories' ? wrapper.style.transform = 'translateX(0)': what == 'main' ? wrapper.style.transform = 'translateX(-25%)': what == 'activity' ? wrapper.style.transform = 'translateX(-50%)' : wrapper.style.transform = 'translateX(-75%)';
 }
+
+const scroll = (to) => {
+    const activityContent = document.getElementsByClassName('activity__content')[0];
+    if(to == 'up') {
+            
+    }
+}
